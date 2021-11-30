27b49840e57208359c3949faa7e5435cb16e66b6
Roberts Ivanovs <roberts.ivanovs@blockvis.com>
2021-11-30 15:16:36 +0200

    feat(converter): can use the prq pancakeswap converter both ways
    By both ways: BUSD->PRQ and PRQ->BUSD

--- contracts/ParsiqPancakeConverter.sol
+++ contracts/ParsiqPancakeConverter.sol
@@ -23,24 +23,19 @@ contract ParsiqPancakeConverter is IConverter {
     /**
      * @notice Constructor for `ParsiqPancakeConverter`
      * @param uniswapRouter - UniswapV2 router implementation. On BSC that would be Pancakeswap.
-     * @param allowedSourceCoin - The ERC20 token that's used for expressing the price of a service
-     * @param allowedTargetCoin - The ERC20 token that's used for paying for the service
+     * @param allowedTokenOne - The ERC20 token that's used for finding the swap pair
+     * @param allowedTargetTwo - The ERC20 token that's used for finding the swap pair
      * @dev The token pair must be pre-deployed and registered on the router!
-     * @dev Instantiate a uniswap router pointer
-     * @dev Find the existing token pair
+     * @dev The contract will find the existing token pair.
      */
     constructor(
         IUniswapV2Router02 uniswapRouter,
-        IERC20 allowedSourceCoin, // <- STABLECOIN (BUSD)
-        IERC20 allowedTargetCoin // <- PRQ
+        IERC20 allowedTokenOne, // <- STABLECOIN (BUSD)
+        IERC20 allowedTargetTwo // <- PRQ
     ) {
         _uniswapRouter = uniswapRouter;
         IUniswapV2Factory uniswapFactory = IUniswapV2Factory(_uniswapRouter.factory());
-        swapPair = IUniswapV2Pair(uniswapFactory.getPair(address(allowedSourceCoin), address(allowedTargetCoin)));
-
-        // Make sure that the `token0` and `token1` mappings are correct
-        require(address(allowedTargetCoin) == swapPair.token0(), Errors.DC_UNSUPPORTED_PAIR);
-        require(address(allowedSourceCoin) == swapPair.token1(), Errors.DC_UNSUPPORTED_PAIR);
+        swapPair = IUniswapV2Pair(uniswapFactory.getPair(address(allowedTokenOne), address(allowedTargetTwo)));
     }
 
     /**
@@ -55,16 +50,21 @@ contract ParsiqPancakeConverter is IConverter {
         uint256 amountInSourceTokens,
         IERC20 target
     ) external view override returns (uint256) {
-        require(address(target) == swapPair.token0(), Errors.DC_UNSUPPORTED_PAIR);
-        require(address(source) == swapPair.token1(), Errors.DC_UNSUPPORTED_PAIR);
-        (uint112 targetReserve, uint112 sourceReserve, uint32 blockTimestampLast) = swapPair.getReserves();
+        address pairToken0 = swapPair.token0();
+        address pairToken1 = swapPair.token1();
 
-        // Formula for conversion: https://ethereum.stackexchange.com/a/103869
-        //      `Y * I / (X + I)`
-        //          I is your input amount of source tokens
-        //          X is the balance of the pool in the source token
-        //          Y is the balance of the pool in the target token
-        return (targetReserve * amountInSourceTokens) / (sourceReserve + amountInSourceTokens);
+        // Initially assume that token0 == target and token1 == source
+        (uint112 targetReserve, uint112 sourceReserve, ) = swapPair.getReserves();
+
+        if (pairToken0 == address(source) && pairToken1 == address(target)) {
+            // Swap token reserves if necessary.
+            (targetReserve, sourceReserve) = (sourceReserve, targetReserve);
+        } else if (pairToken0 != address(target) || pairToken1 != address(source)) {
+            // Received tokens that are not registered on the pair.
+            revert(Errors.DC_UNSUPPORTED_PAIR);
+        }
+
+        return estimateConvertWithReserves(sourceReserve, amountInSourceTokens, targetReserve);
     }
 
     /**
@@ -77,5 +77,21 @@ contract ParsiqPancakeConverter is IConverter {
     ) external pure override returns (uint256) {
         require(address(source) == address(target), Errors.DC_UNSUPPORTED_PAIR);
         return amountInSourceTokens;
+    }
+
+    /**
+     * @notice Return price estimations on given token reserves
+     * @dev Formula for conversion: https://ethereum.stackexchange.com/a/103869
+     *      `Y * I / (X + I)`
+     *          I is your input amount of source tokens
+     *          X is the balance of the pool in the source token
+     *          Y is the balance of the pool in the target token
+     */
+    function estimateConvertWithReserves(
+        uint256 sourceReserve,
+        uint256 amountInSourceTokens,
+        uint256 targetReserve
+    ) internal pure returns (uint256) {
+        return (targetReserve * amountInSourceTokens) / (sourceReserve + amountInSourceTokens);
     }
 }
