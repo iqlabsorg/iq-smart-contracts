4829bcf290cf8f86add0f337816817569d651a6a
Roberts Ivanovs <roberts.ivanovs@blockvis.com>
2021-12-06 13:21:35 +0200

    feat(converter): initial implementation for the uniswap interface converter

--- contracts/ParsiqPancakeConverter.sol
+++ contracts/ParsiqPancakeConverter.sol
@@ -7,11 +7,12 @@
 
 pragma solidity 0.8.4;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+
 import "./interfaces/IConverter.sol";
 import "./libs/Errors.sol";
-import "./libs/uniswap-v2/IUniswapV2Router02.sol";
-import "./libs/uniswap-v2/IUniswapV2Pair.sol";
-import "./libs/uniswap-v2/IUniswapV2Factory.sol";
 
 /**
  * Pancakeswap converter for estimating token prices.
@@ -49,23 +50,12 @@ contract ParsiqPancakeConverter is IConverter {
         IERC20 source,
         uint256 amountInSourceTokens,
         IERC20 target
-    ) external view override returns (uint256) {
+    ) public view override returns (uint256) {
         if (address(source) == address(target)) return amountInSourceTokens;
-
-        address pairToken0 = swapPair.token0();
-        address pairToken1 = swapPair.token1();
-
-        // Initially assume that token0 == target and token1 == source
-        (uint112 targetReserve, uint112 sourceReserve, ) = swapPair.getReserves();
-
-        if (pairToken0 == address(source) && pairToken1 == address(target)) {
-            // Swap token reserves if necessary.
-            (targetReserve, sourceReserve) = (sourceReserve, targetReserve);
-        } else if (pairToken0 != address(target) || pairToken1 != address(source)) {
-            // Received tokens that are not registered on the pair.
-            revert(Errors.DC_UNSUPPORTED_PAIR);
-        }
-
+        (uint112 targetReserve, uint112 sourceReserve, ) = retrieveReservesInSourceTargetOrder(
+            address(source),
+            address(target)
+        );
         return estimateConvertWithReserves(sourceReserve, amountInSourceTokens, targetReserve);
     }
 
@@ -76,9 +66,43 @@ contract ParsiqPancakeConverter is IConverter {
         IERC20 source,
         uint256 amountInSourceTokens,
         IERC20 target
-    ) external pure override returns (uint256) {
-        require(address(source) == address(target), Errors.DC_UNSUPPORTED_PAIR);
-        return amountInSourceTokens;
+    ) external override returns (uint256) {
+        if (address(source) == address(target)) return amountInSourceTokens;
+
+        (uint112 targetReserve, uint112 sourceReserve, bool token0IsTarget) = retrieveReservesInSourceTargetOrder(
+            address(source),
+            address(target)
+        );
+        uint256 amountInTargetTokens = (targetReserve / sourceReserve) * amountInSourceTokens;
+
+        // https://docs.uniswap.org/protocol/V2/reference/smart-contracts/pair#swap-1
+        address[] memory path = new address[](2);
+        if (token0IsTarget) {
+            path[0] = address(source);
+            path[1] = address(target);
+        } else {
+            path[0] = address(target);
+            path[1] = address(source);
+        }
+
+        // 0. (must be done before) Approve: msg.sender -> converter
+        // 1. Transfer: msg.sender -> converter
+        // 2. Approve: covnerter -> uniswap router
+        IERC20(path[0]).transferFrom(msg.sender, address(this), amountInTargetTokens);
+        IERC20(path[0]).approve(address(_uniswapRouter), amountInTargetTokens);
+
+        // https://docs.uniswap.org/protocol/V2/reference/smart-contracts/router-02#swapexacttokensfortokens
+        // Swaps an exact amount of input tokens for as many output tokens as possible, along the route determined by the path.
+        // The first element of path is the input token, the last is the output token, and any intermediate elements represent
+        // intermediate pairs to trade through (if, for example, a direct pair does not exist).
+        uint256[] memory resultAmounts = _uniswapRouter.swapExactTokensForTokens(
+            amountInSourceTokens,
+            amountInTargetTokens,
+            path,
+            msg.sender,
+            block.timestamp
+        );
+        return resultAmounts[1];
     }
 
     /**
@@ -95,5 +119,30 @@ contract ParsiqPancakeConverter is IConverter {
         uint256 targetReserve
     ) internal pure returns (uint256) {
         return (targetReserve * amountInSourceTokens) / (sourceReserve + amountInSourceTokens);
+    }
+
+    function retrieveReservesInSourceTargetOrder(address source, address target)
+        internal
+        view
+        returns (
+            uint112 targetReserve,
+            uint112 sourceReserve,
+            bool token0IsTarget
+        )
+    {
+        // Initially assume that token0 == target and token1 == source
+        address targetToken = swapPair.token0();
+        address sourceToken = swapPair.token1();
+        (targetReserve, sourceReserve, ) = swapPair.getReserves();
+        token0IsTarget = true;
+
+        if (targetToken == address(source) && sourceToken == address(target)) {
+            // Swap token reserves if necessary.
+            (targetReserve, sourceReserve) = (sourceReserve, targetReserve);
+            token0IsTarget = false;
+        } else if (targetToken != address(target) || sourceToken != address(source)) {
+            // Received tokens that are not registered on the pair.
+            revert(Errors.DC_UNSUPPORTED_PAIR);
+        }
     }
 }
--- test/integration/parsiq_pancake_converter.test.ts
+++ test/integration/parsiq_pancake_converter.test.ts
@@ -1,3 +1,4 @@
+import { BigNumber } from '@ethersproject/bignumber';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signers';
 import { expect } from 'chai';
 import hre, { ethers } from 'hardhat';
@@ -11,7 +12,7 @@ import {
   IUniswapV2Pair,
   IUniswapV2Pair__factory,
 } from '../../typechain';
-import { resetFork } from '../utils';
+import { impersonate, resetFork } from '../utils';
 
 const ONE_TOKEN = 10n ** 18n;
 
@@ -24,8 +25,16 @@ const priceOfServiceInBusd = ONE_TOKEN * 10n; // 10 BUSD
 // NOTE https://ethereum.stackexchange.com/a/103869
 const performConversion = (source: bigint, amount: bigint, target: bigint) => (target * amount) / (source + amount);
 
+const humanReadableToken = (source: BigNumber) => {
+  const sourceAsBigInt = BigInt(source.toString());
+  // NOTE: Ignore rounding.
+  const whole = sourceAsBigInt / ONE_TOKEN;
+  const residual = (sourceAsBigInt % ONE_TOKEN).toString().slice(0, 1);
+  return `${whole}.${residual}`;
+};
+
 /// Supposed to be run as a fork of BSC chain.
-describe('ParsiqPancakeConverter', function () {
+describe.only('ParsiqPancakeConverter', function () {
   let busd: ERC20;
   let prq: ERC20;
   let swapPair: IUniswapV2Pair;
@@ -61,67 +70,87 @@ describe('ParsiqPancakeConverter', function () {
   });
   describe('Estimate convert', () => {
     it('should correctly estimate the required PRQ tokens', async () => {
-      const estimatedPriceInPRQ = BigInt(
-        (await converter.estimateConvert(busd.address, priceOfServiceInBusd, prq.address)).toString()
-      );
-
       const data = await swapPair.getReserves();
       const fetchedTargetReserve = BigInt(data.reserve0.toString());
       const fetchedSourceReserve = BigInt(data.reserve1.toString());
 
-      const expectedPriceInPRQ = performConversion(fetchedSourceReserve, priceOfServiceInBusd, fetchedTargetReserve);
-
-      expect(estimatedPriceInPRQ).to.equal(expectedPriceInPRQ, 'The prices do not match!');
-
-      // Sanity check (mostly just for "visual" confirmation)
-      // NOTE: Ignore rounding.
-      const whole = estimatedPriceInPRQ / ONE_TOKEN;
-      const residual = (estimatedPriceInPRQ % ONE_TOKEN).toString().slice(0, 3);
-      expect(`${whole}.${residual}`).to.equal('29.778', `The prices do not match the expected one at block 9346625!`);
-    });
-    it('should correctly estimate the required BUSD tokens', async () => {
       const estimatedPriceInPRQ = BigInt(
-        (await converter.estimateConvert(prq.address, priceOfServiceInBusd, busd.address)).toString()
+        (await converter.estimateConvert(busd.address, priceOfServiceInBusd, prq.address)).toString()
       );
 
+      const expectedPriceInPRQ = performConversion(fetchedSourceReserve, priceOfServiceInBusd, fetchedTargetReserve);
+      expect(estimatedPriceInPRQ.toString()).to.equal(expectedPriceInPRQ.toString(), 'The prices do not match!');
+      // Sanity check (mostly just for "visual" confirmation)
+      expect(humanReadableToken(BigNumber.from(estimatedPriceInPRQ))).to.equal(
+        '29.7',
+        `The prices do not match the expected one at block 9346625!`
+      );
+    });
+    it('should correctly estimate the required BUSD tokens', async () => {
       const data = await swapPair.getReserves();
       const fetchedTargetReserve = BigInt(data.reserve1.toString());
       const fetchedSourceReserve = BigInt(data.reserve0.toString());
 
+      const estimatedPriceInBUSD = BigInt(
+        (await converter.estimateConvert(prq.address, priceOfServiceInBusd, busd.address)).toString()
+      );
+
       const expectedPriceInPRQ = performConversion(fetchedSourceReserve, priceOfServiceInBusd, fetchedTargetReserve);
-
-      expect(estimatedPriceInPRQ).to.equal(expectedPriceInPRQ, 'The prices do not match!');
-
+      expect(estimatedPriceInBUSD).to.equal(expectedPriceInPRQ, 'The prices do not match!');
       // Sanity check (mostly just for "visual" confirmation)
-      // NOTE: Ignore rounding.
-      const whole = estimatedPriceInPRQ / ONE_TOKEN;
-      const residual = (estimatedPriceInPRQ % ONE_TOKEN).toString().slice(0, 3);
-      expect(`${whole}.${residual}`).to.equal('3.357', `The prices do not match the expected one at block 9346625!`);
+      expect(humanReadableToken(BigNumber.from(estimatedPriceInBUSD))).to.equal(
+        '3.3',
+        `The prices do not match the expected one at block 9346625!`
+      );
     });
     it('should not allow estimation between a registered and an unregistered token', async () => {
-      const WETH = await router.WETH();
       // WETH is not registered
+      const WETH = await router.WETH();
+
       await expect(converter.estimateConvert(WETH, priceOfServiceInBusd, busd.address)).to.be.revertedWith('36');
       await expect(converter.estimateConvert(prq.address, priceOfServiceInBusd, WETH)).to.be.revertedWith('36');
     });
     it('should allow estimation when source and target are the same, even when unregistered', async () => {
-      const WETH = await router.WETH();
       // WETH is not registered in the constructor!
+      const WETH = await router.WETH();
+
       const estimatedPrice = await converter.estimateConvert(WETH, priceOfServiceInBusd, WETH);
+
       expect(estimatedPrice).to.equal(priceOfServiceInBusd.toString());
     });
     it('should allow to use the same registered token for `target` and `source` fields', async () => {
       const estimatedPrice = await converter.estimateConvert(busd.address, priceOfServiceInBusd, busd.address);
+
       expect(estimatedPrice).to.equal(priceOfServiceInBusd.toString());
     });
   });
   describe('Convert', () => {
-    it('should revert on conversion when both tokens are not equal', async () => {
-      await expect(converter.convert(busd.address, ONE_TOKEN, prq.address)).to.be.revertedWith('36');
+    const BUSD_HOLDER = '0x8c7de13ecf6e92e249696defed7aa81e9c93931a';
+    const PRQ_HOLDER = '0xfaa9721d51c49f0ca7e82203d7914c9726b5ccab'; // (note: this the IQ protocols address, but that does not matter for these tests )
+
+    beforeEach(async () => {
+      console.log('busd.balanceOf(BUSD_HOLDER)', (await busd.balanceOf(BUSD_HOLDER)).toString());
+      console.log('prq.balanceOf(PRQ_HOLDER)', (await prq.balanceOf(PRQ_HOLDER)).toString());
+
+      // Sanity check to make sure the addresses do not contain the other token type
+      const balanceOfPRQforBUSDHolder = await prq.balanceOf(BUSD_HOLDER);
+      const balanceOfBUSDforPRQHolder = await busd.balanceOf(PRQ_HOLDER);
+      expect(balanceOfBUSDforPRQHolder).to.equal(0);
+      expect(balanceOfPRQforBUSDHolder).to.equal(0);
+
+      await hre.network.provider.send('hardhat_setBalance', [PRQ_HOLDER, '0x1000000000000000000000000000000000000000']);
     });
-    it('should not revert on conversion using equal tokens', async () => {
-      const conversionResult = await converter.convert(prq.address, ONE_TOKEN, prq.address);
-      expect(conversionResult).to.equal(ONE_TOKEN);
+
+    it('should allow conversion token A -> token B when both tokens are registered', async () => {
+      await impersonate(hre, BUSD_HOLDER);
+      const busdHolder = await ethers.getSigner(BUSD_HOLDER);
+      const amountToApprove = await converter.estimateConvert(busd.address, priceOfServiceInBusd, prq.address);
+      await busd.connect(busdHolder).approve(converter.address, amountToApprove);
+
+      await converter.connect(busdHolder).convert(busd.address, priceOfServiceInBusd, prq.address);
+
+      const balanceOfPRQ = await prq.balanceOf(BUSD_HOLDER);
+      expect(humanReadableToken(balanceOfPRQ)).to.equal('29.7');
     });
   });
 });
